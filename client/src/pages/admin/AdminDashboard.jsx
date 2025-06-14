0eec1820dd7cc898359952cdd42dd8f9bf438aa9
amit <amitbhandwalkar66@gmail.com>
2025-06-15 01:19:55 +0530

    fix cookies for mobile

--- client/src/pages/admin/AdminDashboard.jsx
+++ client/src/pages/admin/AdminDashboard.jsx
@@ -11,7 +11,6 @@ import PostAddIcon from '@mui/icons-material/PostAdd';
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-import CheckCookies from '../../components/CheckCookies'
 
 const AdminDashboard = () => {
 
@@ -52,10 +51,8 @@ useEffect(() => {
 
   return (
     <div className='AdminDashboard'>
-
-      <CheckCookies/>
      
-     {/* <div className="widgets">
+     <div className="widgets">
       
       <Widgets title={"New Orders"} link={"todayorders"} linktext={"see today orders"} 
       count={adminData.newOrders} icon={ <PostAddIcon className='icon' />} newPercentageChange={adminData.newPercentageChange} />
@@ -73,7 +70,7 @@ useEffect(() => {
      <div className='charts' >
         <Featured OrderStatusData={adminData.OrderStatusSummary} />
         <Chart OrderData={adminData.orderChartData}/>
-     </div> */}
+     </div>
   
     </div>
   )
